c81a9febb312a817b63ec5e5dca78aa369fda313
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-07-16 12:02:03 -0400

    pre-render fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,11 +6,11 @@ export default function Header() {
     <div className={"header"}>
       <Navbar bf={"light"} expand="md">
         <Container>
-          <Navbar.Brand href="/"><Image width="130px" alt="hypnotic eye" src="https://www.datocms-assets.com/76639/1657897987-hypnotic-eye.png" /></Navbar.Brand>
+          <Navbar.Brand href="#"><Image width="130px" alt="hypnotic eye" src="https://www.datocms-assets.com/76639/1657897987-hypnotic-eye.png" /></Navbar.Brand>
           <Navbar.Toggle />
           <Navbar.Collapse>
             <Nav className="m-auto">
-              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="#">Home</Nav.Link>
               <Nav.Link href="#services">Services</Nav.Link>
               <Nav.Link href="#about">About</Nav.Link>
             </Nav>
