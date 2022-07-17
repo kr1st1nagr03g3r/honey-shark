a16ab52284e2e22ec828e21b680a1ffd5b23a1e6
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-07-17 11:31:15 -0400

    nav debug

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../styles/globals.css"
-import Nav from "../src/components/Nav"
-
 
 function MyApp({ Component, pageProps }) {
-  return (
-  <> 
-  <Nav />
-  <Component {...pageProps} />
-  </> 
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
