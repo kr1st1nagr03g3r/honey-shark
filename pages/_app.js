66413d0cd4a68381b6235afb77992638c159e56e
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-07-17 12:39:41 -0400

    🐎

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+// This was where justyn told me to put a nav on every page lol
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import "@/styles/globals.css"
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+
 import "bootstrap/dist/css/bootstrap.min.css"
-import "@/styles/globals.css"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
