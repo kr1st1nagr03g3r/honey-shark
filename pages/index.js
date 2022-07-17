e6c4c3825cefef9fd0d85783fcd8993d35242f81
kr1st1nagr03g3r <slowest.barges_0i@icloud.com>
2022-07-17 11:53:43 -0400

    @components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from "next/head"
-import { getHome } from "/util/api"
-import Hero from "/src/components/Hero"
+import { getHome } from "util/api"
+import Hero from "@/components/Hero"
+import Nav from "@/components/Nav"
 
 export default function Index({ homeInfo }) {
     return (
         <>
             <Head>  <title>KG</title></Head>
-
+            <Nav />
             <Hero heroTitle={homeInfo.title} heroSubtitle={homeInfo.heroSubtitle} heroImage={homeInfo.heroImage} />
             <h1>{homeInfo.heroSubtitle}</h1>
         </>
